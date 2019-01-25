83d0905c68bb7c5d6642ce2c3b6f82d3be2b6a92
sqp0627 <1551936315@qq.com>
2019-01-25 17:24:59 +0800

    首页完成

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.less'
-import { Row, Col } from 'antd'
+import {Row, Col} from 'antd'
 import Header from './src/components/Header'
 import Footer from './src/components/Footer'
 import NavLeft from './src/components/NavLeft'
 import Home from './src/pages/home';
 import {BrowserRouter, Route} from 'react-router-dom'
-import Login from './src/pages/login';
+import Login from "./src/pages/login";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Row className="container">
-        <Col span={4} className="navLeft">
-          <NavLeft />
-        </Col>
-        <Col span={20} className="main">
-          <Header />
-          <Row className="content">
-            <Route path='/' exact component={Home}></Route>
-            <Route path='/login' exact component={Login}></Route>
-          </Row>
-          <Footer />
-        </Col>
-      </Row>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Row className="container">
+                <Col span={4} className="navLeft">
+                    <NavLeft/>
+                </Col>
+                <Col span={20} className="main">
+                    <Header/>
+                    <Row className="content">
+                        <Route path={'/'} exact component={Home}/>
+                        <Route path={'/login'} exact component={Login}/>
+                    </Row>
+                    <Footer/>
+                </Col>
+            </Row>
+        </BrowserRouter>
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react'
-import { Row, Menu } from 'antd'
+import React, {Component} from 'react'
+import {Row, Menu} from 'antd'
 import menuList from './menuConfig'
 import './index.less'
 
 const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends Component {
-  componentWillMount() {
-    const menuTreeNode = this.renderMenu(menuList);
+    componentWillMount() {
+        const menuTreeNode = this.renderMenu(menuList);
 
-    this.setState({
-      menuTreeNode
-    })
-  }
+        this.setState({
+            menuTreeNode
+        })
+    }
 
-  // 菜单渲染
-  renderMenu = data => {
-    return data.map(item => {
-      if (item.children) {
+    // 菜单渲染
+    renderMenu = data => {
+        return data.map(item => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return (
+                <Menu.Item title={item.title} key={item.key}>
+                    {item.title}
+                </Menu.Item>
+            )
+        })
+    };
+
+    render() {
         return (
-          <SubMenu title={item.title} key={item.key}>
-            {this.renderMenu(item.children)}
-          </SubMenu>
+            <Row className="logo">
+                <img src="../../../public/assets/logo-ant.svg" alt=""/>
+                <h1>Back MS</h1>
+                <Menu theme="dark">{this.state.menuTreeNode}</Menu>
+            </Row>
         )
-      }
-      return (
-        <Menu.Item title={item.title} key={item.key}>
-          {item.title}
-        </Menu.Item>
-      )
-    })
-  }
-
-  render() {
-    return (
-      <Row className="logo">
-        <img src="../../../public/assets/logo-ant.svg" alt="" />
-        <h1>Back MS</h1>
-        <Menu theme="dark">{this.state.menuTreeNode}</Menu>
-      </Row>
-    )
-  }
+    }
 }
